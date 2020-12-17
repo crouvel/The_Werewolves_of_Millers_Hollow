966b8f340277d6761aa8b4c463d455f8298006d2
tiffany-dumaire <44428702+tiffany-dumaire@users.noreply.github.com>
2020-12-17 23:56:02 +0100

    passwordField for login

--- The Werewolves of Millers Hollow/src/gui/controller/LoginController.java
+++ The Werewolves of Millers Hollow/src/gui/controller/LoginController.java
@@ -8,7 +8,6 @@ package gui.controller;
  */
 import java.io.IOException;
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import application.TheWerewolvesOfMillersHollow;
