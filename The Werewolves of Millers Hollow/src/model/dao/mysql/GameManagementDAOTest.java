118b77ef056fa39ebc7e6786db5208abdbd4c52f
Clarence Rouvel <clarence.rouvel@gmail.com>
2021-01-11 03:11:57 +0100

    commented test

--- The Werewolves of Millers Hollow/src/model/dao/mysql/GameManagementDAOTest.java
+++ The Werewolves of Millers Hollow/src/model/dao/mysql/GameManagementDAOTest.java
@@ -1,40 +1,45 @@
 package model.dao.mysql;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Imported classes and libraries.
+ */
 import java.sql.SQLException;
-
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-
 import businesslogic.domain.Game;
 import model.dao.factory.AbstractFactoryDAO;
 
+/**
+ * GameManagementDAOTest class.
+ * @author clare
+ */
 class GameManagementDAOTest {
 
-	/*
+	/**
 	 * Checks if the game is retrieved from the database
+	 * @return void.
 	 */
 	@Test
 	void testGetGame() {
 		AbstractFactoryDAO factory = AbstractFactoryDAO.getInstance();
-	       try {
-	        GameManagementDAO gameManagementDAO  = factory.createGameManagementDAO();
-	        
-	        /**
-	         * Game with id 166 exists in the database, supposed to return Succes.
-	         */
-	        Game game = gameManagementDAO.getGame(166);
-	        if (game != null) {
-                System.out.println("createSprintBacklog: Success");
-            }
-            else{
-                System.out.println("createSprintBacklog: Failed, function tells us something went wrong during the procedure");
-            }
-	       
-	       }catch(SQLException e) {
-	    	   e.printStackTrace();
-	       }
-	    }
+		try {
+			GameManagementDAO gameManagementDAO  = factory.createGameManagementDAO();
+
+			/**
+			 * Game with id 166 exists in the database, supposed to return Success.
+			 */
+			Game game = gameManagementDAO.getGame(166);
+			if (game != null) {
+				System.out.println("getGame: Success");
+			}
+			else{
+				System.out.println("getGame: Failed, function tells us something went wrong during the procedure");
+			}
+
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
+
+}
 
